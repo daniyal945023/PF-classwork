54ffdeea0dee1d46d266a4fa809fbaa0d29d985c
Muhammad Daniyal Siddiqui <157977442+daniyal945023@users.noreply.github.com>
2026-02-12 10:36:51 +0500

    Create oop.cpp

---
+++ oop.cpp
@@ -0,0 +1,59 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Marketplace{
+    string marketPlaceName;
+    string users[];
+    string VehicleListings[];
+    string messages[];
+    int totalUsers;
+    
+    
+};
+
+class User{
+    
+}
+
+
+class Buyer{
+    
+};
+
+class Seller{
+    
+};
+
+class Vehicle{
+    
+};
+
+class Car{
+    
+};
+
+class Bike{
+    
+};
+
+class VehicleListing{
+    
+}
+
+class Messaging{
+    
+}
+
+class Review
+
+
+
+
+
+int main() {
+    
+
+    return 0;
+}
